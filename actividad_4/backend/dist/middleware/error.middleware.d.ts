165b9e8914f1bffe6fe1d890efc8541f96cd8aad
Francisco Briones <franciscobrioneslavados@gmail.com>
2026-04-16 16:04:33 -0400

    Remove actividad_4 from actividad_3 branch

--- actividad_4/backend/dist/middleware/error.middleware.d.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response, NextFunction } from "express";
-export interface ApiError extends Error {
-    statusCode?: number;
-    message: string;
-}
-export declare function errorHandler(err: ApiError, _req: Request, res: Response, _next: NextFunction): void;
-export declare function notFoundHandler(_req: Request, res: Response): void;
-//# sourceMappingURL=error.middleware.d.ts.map
