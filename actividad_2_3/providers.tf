0fe073def95484d75ebb3319558af1f8fbb2c921
Francisco Briones <franciscobrioneslavados@gmail.com>
2026-04-26 13:22:34 -0400

    first commit

---
+++ actividad_2_3/providers.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_version = ">= 1.5.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.4"
+    }
+  }
+}
+
+provider "aws" {
+  region  = var.aws_region
+  profile = "duoc"
+
+  default_tags {
+    tags = {
+      Project     = var.project_name
+      Environment = var.environment
+      ManagedBy   = "Terraform"
+      OwnerName   = var.owner_name
+    }
+  }
+}
